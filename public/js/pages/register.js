535eac37ab1e9fa4975c5ee1060c0dabe8e4b254
Nikhil <nikhilpriyadarshan1234@gmail.com>
2024-05-24 00:36:02 +0530

    final register commit

--- public/js/pages/register.js
+++ public/js/pages/register.js
@@ -1,9 +1,9 @@
-import * as client from "../client/client";
+import * as client from "../client/client.js";
 
 document.addEventListener('DOMContentLoaded', () => {
     const form = document.getElementById('regForm');
   
-    form.addEventListener('submit', (event) => {
+    form.addEventListener('submit',async (event) => {
         event.preventDefault();
 
         const formData = {
@@ -13,8 +13,20 @@ document.addEventListener('DOMContentLoaded', () => {
             role: document.querySelector('input[name="role"]:checked').value
         };
     
-        client.postUserDataToServer(formData);
-        
+        //client.postUserDataToServer(formData);
+        try {
+            await client.postUserDataToServer(formData);
+            console.log('Form submitted successfully');
+            // Show success message
+            alert('User successfully registered');
+            // Redirect to login page after showing the alert
+            window.location.href = '/login';
+        } catch (error) {
+            console.error('Error submitting form:', error);
+            // Show error message (optional)
+            alert('Error registering user. Please try again.');
+        }
+
 
         form.reset();
     });
