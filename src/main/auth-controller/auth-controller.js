bdb070de07bfe12981bd3560181a129ce569f32b
Nikhil <nikhilpriyadarshan1234@gmail.com>
2024-05-27 14:53:47 +0530

    async await in auth controller

--- src/main/auth-controller/auth-controller.js
+++ src/main/auth-controller/auth-controller.js
@@ -1,10 +1,10 @@
 const dbController = require("../db-controller/db-controller");
 class authController {
-    authenticateUser(formData){
+    async authenticateUser(formData){
         
         const email = formData.email;
         const password=formData.password;
-        const user = dbController.findUserByEmail(email);
+        const user =await dbController.findUserByEmail(email);
         let role =user.role;
         if (!user || user.password!==password) {
             return {success:false,role:"null"};
