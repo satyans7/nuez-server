570cbd1568f691a1b5e400a8945530f61622f09d
kavyas <kaveeyasharma@gmail.com>
2024-05-30 15:59:33 +0530

    device profile initial commit

---
+++ public/js/pages/deviceProfile.js
@@ -0,0 +1,9 @@
+function editProfile() {
+    // Functionality to edit the device profile
+    console.log("Edit Profile clicked");
+}
+
+function deleteDevice() {
+    // Functionality to delete the device
+    console.log("Delete Device clicked");
+}
