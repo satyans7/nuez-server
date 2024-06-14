7b5a80f1b2fd0774310a75c46950c7fa6f94b6cc
Priyansu Mohanty <priyansu.iitism@gmail.com>
2024-06-15 02:03:00 +0530

    gitignore and firmwarescript

--- firmwareScript.sh
+++ firmwareScript.sh
@@ -1,2 +1,24 @@
-#!/bin/sh
-echo "Hello, World!"
+#!/bin/bash
+
+# Variables
+LOCAL_REPO_PATH="src/main/local-repo"
+GITHUB_REPO_URL="https://github.com/priyansu1703/testFile.git"
+
+# Function to update the repository
+update_repo() {
+  if [ ! -d "$LOCAL_REPO_PATH/.git" ]; then
+    # Clone the repository if it doesn't exist
+    echo "Cloning the repository..."
+    git clone "$GITHUB_REPO_URL" "$LOCAL_REPO_PATH"
+  else
+    # Pull the latest changes if the repository exists
+    echo "Pulling the latest changes..."
+    cd "$LOCAL_REPO_PATH" || exit
+    git pull
+  fi
+}
+
+# Run the update function
+update_repo
+
+
