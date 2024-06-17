4ef59bc356e956b5c21d660d5bfe258f810559d1
Priyansu Mohanty <priyansu.iitism@gmail.com>
2024-06-17 11:24:22 +0530

    sourece code change

--- sourceCodeScript.sh
+++ sourceCodeScript.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 update_repo(){
-  # echo "pulling the latest changes"
-  # git pull
-  touch working.txt
+  echo "pulling the latest changes"
+  git pull
+  # touch working.txt
 }
 # Run the update function
 update_repo
