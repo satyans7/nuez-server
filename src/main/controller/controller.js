1266b3a69de805a903352c49c31d0d2103ba93c5
Priyansu Mohanty <priyansu.iitism@gmail.com>
2024-07-04 17:52:47 +0530

    ip change

--- src/main/controller/controller.js
+++ src/main/controller/controller.js
@@ -433,7 +433,7 @@ class Controller {
     const allDeviceData = await this.fetchAllDevices();
     let deviceIds = Object.keys(allDeviceData);
     deviceIds.forEach(deviceId => {
-      const apiUrl = `http://192.168.33.250:4000/device-info/${deviceId}`;
+      const apiUrl = `http://206.189.138.34:4000/device-info/${deviceId}`;
 
       // Sanitize deviceId to remove characters not suitable for filenames
       const sanitizedDeviceId = deviceId.replace(/[^a-z0-9]/gi, '-'); // Replace non-alphanumeric characters with hyphen
