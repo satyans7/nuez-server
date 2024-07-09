36578c3b420a6ceded594e3ae0664793753827ee
Brahmprakash Gautam <brahmprakash.gautam03@gmail.com>
2024-07-09 21:03:18 +0530

    device owner added

--- public/js/pages/deviceProfile.js
+++ public/js/pages/deviceProfile.js
@@ -51,6 +51,7 @@ async function initializeProfilePage() {
     const currentId = getCurrentDevice();
     const devicesData = await getDevicesData();
     const particularDeviceData = devicesData[currentId];
+    console.log(currentId)
 
     if (particularDeviceData) {
         DEVICE_ID = currentId;
@@ -69,11 +70,13 @@ async function initializeProfilePage() {
 
 // Function to populate the device card with data
 function populateDeviceCard(data) {
+    console.log(data.owner);
     document.getElementById('device-name').textContent = `Name: ${data.name}`;
     document.getElementById('device-location').textContent = `Location: ${data.location}`;
-    document.getElementById('device-totalConsumption').textContent = `Total Consumption: ${data.totalConsumption}`;
-    document.getElementById('device-status').textContent = `Status: ${data.status}`;
-    document.getElementById('device-registrationDate').textContent = `Registration Date: ${data.registrationDate}`;
+    //document.getElementById('device-totalConsumption').textContent = `Total Consumption: ${data.totalConsumption}`;
+    //document.getElementById('device-status').textContent = `Status: ${data.status}`;
+    document.getElementById('device-owner').textContent = `Owner: ${data.owner}`;
+    //document.getElementById('device-registrationDate').textContent = `Registration Date: ${data.registrationDate}`;
 }
 
 // Function to add Edit and Delete buttons to the device card
